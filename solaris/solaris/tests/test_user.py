3319bb4bdec8e56b19a6efd3f9475a6ba3c29503
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2023-09-05 21:00:21 -0300

    test_quantity_connected_users

--- solaris/solaris/tests/test_user.py
+++ solaris/solaris/tests/test_user.py
@@ -1,9 +1,10 @@
-from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
 
 
 class TestUser(TestCase):
+    USERS_EXPECTED = 100
+
     def setUp(self):
         """Implementa algumas ações antes de iniciar cada teste."""
         self.cliennt = Client()
@@ -21,17 +22,20 @@ class TestUser(TestCase):
         )
         self.assertTrue(self.usuario.is_authenticated)
 
+    def test_quantity_connected_users(self):
+        """Testa a quantidade de usuários conectados."""
+        for i in range(self.USERS_EXPECTED):
+            username = f"usuario{i}"
+            email = f"email{i}@example.com"
+            password = f"password-example{i}"
 
-# Não funcionais:
-# [RNF001] Atividade de login
-# O sistema de guardar a atividade de login dos usuários pela última vez de uso
-# Ok, temos uns campos no admin para isso, podemos só tirar alguns prints.
-
-# [RNF002] Logs do usuário
-# O sistema deve registrar os logs do usuário.
-# Ok, temos menus no admin para isso, podemos só tirar alguns prints.
-
-# RNF005] Quantidade de usuário conectados
-# O sistema deve suportar um número máximo de 100 usuários conectados simultaneamente.  # noqa E501
-# Talvez até consigamos logar 100 usuários, mas teremos que limitar o
-# número máximo para 100? Meio estranho, não acham?
+            User.objects.create(
+                username=username,
+                email=email,
+                password=password,
+            )
+            self.client.login(
+                username=email,
+                password=password,
+            )
+            self.assertTrue(self.usuario.is_authenticated)
